c35c32e64dc86b10fab3e8eedf3bd265e3a09f53
Jeroen de Bruijn <vidavidorra@gmail.com>
2015-11-18 20:27:06 +0100

    #8: Created files to read the configuration bits. See comment in #8 for example.

---
+++ source/LibraryFiles/getPICConfigurationBits.h
@@ -0,0 +1,249 @@
+/*******************************************************************************
+ * Begin of file getPICConfigurationBits.h
+ * Author: jdebruijn
+ * Created on November 16, 2015, 4:10 PM
+ * 
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+ * 
+ * Copyright (c) 2015 Jeroen de Bruijn <vidavidorra@gmail.com>
+ * 
+ * This file is part of LedCube which is released under The MIT License (MIT).
+ * For full license details see file "main.c" or "LICENSE.md" or go to
+ * https://opensource.org/licenses/MIT
+ * 
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~**/
+/** @file
+ * @brief Gets the configuration bits that are set in the microcontroller.
+ * 
+ * @note    This file is designed for the PIC24FJ64GA004, so the documentation
+ * of the configuration bits can differ if a different PIC is used. The compiler
+ * will give a compiler warning if this file is not used with a PIC24FJ64GA004.
+ * Also the structures for the Flash Configuration Words are based on that PIC.
+ * Therefore it's recommended to check whether the documentation and structure
+ * layout is compatible to the used PIC microcontroller.
+ * @bug     @ref _getPICConfigurationBits_readLatch gives a compiler warning
+ * "warning: control reaches end of non-void function", because a value is not
+ * explicitly returned from the function.
+ * 
+ ******************************************************************************/
+
+#ifndef GETPICCONFIGURATIONBITS_H
+#define GETPICCONFIGURATIONBITS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*******************************************************************************
+ * Condition checks
+ ******************************************************************************/
+#ifndef __PIC24FJ64GA004__
+# warning "This file is designed for the PIC24FJ64GA004, so the documentation"
+# warning "of the configuration bits can differ if a different PIC is used."
+# warning "Also the structures for the Flash Configuration Words are based on"
+# warning "that PIC. Therefore it's recommended to check the documentation."
+#endif
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <xc.h>
+#include <stdbool.h>
+#include "BitOperations.h"
+#include "Uart.h"
+
+/*******************************************************************************
+ * Defines
+ ******************************************************************************/
+#ifndef PRINTF
+/** Wrapper for PRINTF to an actual printf function, in this case @ref
+ * Uart1_printf.
+ */
+# define PRINTF Uart1_printf
+#endif
+
+#ifdef __CONFIG1_BASE
+/** Base address for Flash Configuration Word 1 */
+# define _CONFIG1_BASE_ADDRESS __CONFIG1_BASE
+#else
+# define _CONFIG1_BASE_ADDRESS 0x000000 /* Dummy base address */
+# error "__CONFIG1_BASE is not defined. Enter the base address of Flash Configuration Word 1 manually."
+# error "For the Configuration Word Addresses @see PIC24FJ64GA004 Family Data Sheet.pdf TABLE 24-1."
+#endif
+
+#ifdef __CONFIG2_BASE
+/** Base address for Flash Configuration Word 1 */
+# define _CONFIG2_BASE_ADDRESS __CONFIG2_BASE
+#else
+# define _CONFIG2_BASE_ADDRESS 0x000000 /* Dummy base address */
+# error "__CONFIG2_BASE is not defined. Enter the base address of Flash Configuration Word 2 manually."
+# error "For the Configuration Word Addresses @see PIC24FJ64GA004 Family Data Sheet.pdf TABLE 24-1."
+#endif
+#define _CONFIG2_BASE_ADDRESS __CONFIG2_BASE
+
+/********** Result descriptions for Flash Configuration Word 1 ************/
+#define JTAGEN_DESC "JTAGEN: JTAG Port Enable bit"
+#define JTAGEN_1 "[JTAGEN_ON] JTAG port is enabled"
+#define JTAGEN_0 "[JTAGEN_OFF] JTAG port is disabled"
+#define GCP_DESC "GCP: General Segment Program Memory Code Protection bit"
+#define GCP_1 "[GCP_OFF] Code protection is disabled"
+#define GCP_0 "[GCP_ON] Code protection is enabled for the entire program memory space"
+#define GWRP_DESC "GWRP: General Segment Code Flash Write Protection bit"
+#define GWRP_1 "[GWRP_OFF] Writes to program memory are allowed"
+#define GWRP_0 "[GWRP_ON] Writes to program memory are disabled"
+#define BKBUG_DESC "!DEBUG: Background Debugger Enable bit"
+#define BKBUG_1 "[BKBUG_OFF] Device resets into Operational mode"
+#define BKBUG_0 "[BKBUG_ON] Device resets into Debug mode"
+#define ICS_DESC "ICS<1:0>: Emulator Pin Placement Select bits"
+#define ICS_11 "[ICS_PGx1] Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1"
+#define ICS_10 "[ICS_PGx2] Emulator EMUC2/EMUD2 pins are shared with PGC2/PGD2"
+#define ICS_01 "[ICS_PGx3] Emulator EMUC3/EMUD3 pins are shared with PGC3/PGD3"
+#define ICS_00 "Reserved; do not use"
+#define FWDTEN_DESC "FWDTEN: Watchdog Timer Enable bit"
+#define FWDTEN_1 "[FWDTEN_ON] Watchdog Timer is enabled"
+#define FWDTEN_0 "[FWDTEN_OFF] Watchdog Timer is disabled"
+#define WINDIS_DESC "WINDIS: Windowed Watchdog Timer Disable bit"
+#define WINDIS_1 "[WINDIS_ON] Standard Watchdog Timer is enabled"
+#define WINDIS_0 "[WINDIS_OFF] Windowed Watchdog Timer is enabled; FWDTEN must be '1'"
+#define FWPSA_DESC "FWPSA: WDT Prescaler Ratio Select bit"
+#define FWPSA_1 "[FWPSA_PR128] Prescaler ratio of 1:128"
+#define FWPSA_0 "[FWPSA_PR32] Prescaler ratio of 1:32"
+#define WDTPS_DESC "WDTPS<3:0>: Watchdog Timer Postscaler Select bits"
+#define WDTPS_1111 "[WDTPS_PS32768] 1:32768"
+#define WDTPS_1110 "[WDTPS_16384] 1:16384"
+#define WDTPS_1101 "[WDTPS_8192] 1:8192"
+#define WDTPS_1100 "[WDTPS_4096] 1:4096"
+#define WDTPS_1011 "[WDTPS_2048] 1:2048"
+#define WDTPS_1010 "[WDTPS_1024] 1:1024"
+#define WDTPS_1001 "[WDTPS_512] 1:512"
+#define WDTPS_1000 "[WDTPS_256] 1:256"
+#define WDTPS_0111 "[WDTPS_128] 1:128"
+#define WDTPS_0110 "[WDTPS_64] 1:64"
+#define WDTPS_0101 "[WDTPS_32] 1:32"
+#define WDTPS_0100 "[WDTPS_16] 1:16"
+#define WDTPS_0011 "[WDTPS_8] 1:8"
+#define WDTPS_0010 "[WDTPS_4] 1:4"
+#define WDTPS_0001 "[WDTPS_2] 1:2"
+#define WDTPS_0000 "[WDTPS_1] 1:1"
+
+/********** Result descriptions for Flash Configuration Word 2 ************/
+#define IESO_DESC "IESO: Internal External Switchover bit"
+#define IESO_1 "[IESO_ON] IESO mode (Two-Speed Start-up) is enabled"
+#define IESO_0 "[IESO_OFF] IESO mode (Two-Speed Start-up) is disabled"
+#define WUTSEL_DESC "WUTSEL<1:0>: Voltage Regulator Standby Mode Wake-up Time Select bits"
+#define WUTSEL_11 "[WUTSEL_LEG] Default regulator start-up time is used"
+#define WUTSEL_01 "[WUTSEL_FST] Fast regulator start-up time is used"
+#define WUTSEL_x0 "Reserved; do not use"
+#define SOSCSEL_DESC "SOSCSEL<1:0>: Secondary Oscillator Power Mode Select bits"
+#define SOSCSEL_11 "[SOSCSEL_SOSC] Default (High Drive Strength) mode"
+#define SOSCSEL_01 "[SOSCSEL_LPSOSC] Low-Power (Low Drive Strength) mode"
+#define SOSCSEL_x0 "Reserved; do not use"
+#define FNOSC_DESC "FNOSC<2:0>: Initial Oscillator Select bits"
+#define FNOSC_111 "[FNOSC_FRCDIV] Fast RC Oscillator with Postscaler (FRCDIV)"
+#define FNOSC_110 "Reserved"
+#define FNOSC_101 "[FNOSC_LPRC] Low-Power RC Oscillator (LPRC)"
+#define FNOSC_100 "[FNOSC_SOSC] Secondary Oscillator (SOSC)"
+#define FNOSC_011 "[FNOSC_PRIPLL] Primary Oscillator with PLL module (XTPLL, HSPLL, ECPLL)"
+#define FNOSC_010 "[FNOSC_PRI] Primary Oscillator (XT, HS, EC)"
+#define FNOSC_001 "[FNOSC_FRCPLL] Fast RC Oscillator with Postscaler and PLL module (FRCPLL)"
+#define FNOSC_000 "[FNOSC_FRC] Fast RC Oscillator (FRC)"
+#define FCKSM_DESC "FCKSM<1:0>: Clock Switching and Fail-Safe Clock Monitor Configuration bits"
+#define FCKSM_1x "[FCKSM_CSDCMD] Clock switching and Fail-Safe Clock Monitor are disabled"
+#define FCKSM_01 "[FCKSM_CSECMD] Clock switching is enabled, Fail-Safe Clock Monitor is disabled"
+#define FCKSM_00 "[FCKSM_CSECME] Clock switching is enabled, Fail-Safe Clock Monitor is enabled"
+#define OSCIOFCN_DESC "OSCIOFCN: OSCO Pin Configuration bit:"
+#define OSCIOFCN_1 "[OSCIOFNC_OFF] OSCO/CLKO/RA3 functions as CLKO (FOSC/2)"    /* If POSCMD<1:0> = 11 or 00: */
+#define OSCIOFCN_0 "[OSCIOFNC_ON] OSCO/CLKO/RA3 functions as port I/O (RA3)"    /* If POSCMD<1:0> = 11 or 00: */
+#define OSCIOFCN_x "OSCIOFCN has no effect on OSCO/CLKO/RA3"                    /* If POSCMD<1:0> = 10 or 01: */
+#define IOL1WAY_DESC "IOL1WAY: IOLOCK One-Way Set Enable bit"
+#define IOL1WAY_1 "[IOL1WAY_ON] The IOLOCK (OSCCON<6>) bit can be set once, provided the unlock sequence has been completed. Once set, the Peripheral Pin Select registers cannot be written to a second time."
+#define IOL1WAY_0 "[IOL1WAY_OFF] The IOLOCK (OSCCON<6>) bit can be set and cleared as needed, provided the unlock sequence has been completed"
+#define I2C1SEL_DESC "I2C1SEL: I2C1 Pin Select bit"
+#define I2C1SEL_1 "[I2C1SEL_PRI] Use default SCL1/SDA1 pins"
+#define I2C1SEL_0 "[I2C1SEL_SEC] Use alternate SCL1/SDA1 pins"
+#define POSCMD_DESC "POSCMD<1:0:> Primary Oscillator Configuration bits"
+#define POSCMD_11 "[POSCMOD_NONE] Primary oscillator is disabled"
+#define POSCMD_10 "[POSCMOD_HS] HS Oscillator mode is selected"
+#define POSCMD_01 "[POSCMOD_XT] XT Oscillator mode is selected"
+#define POSCMD_00 "[POSCMOD_EC] EC Oscillator mode is selected"
+
+/*******************************************************************************
+ * Macros
+ ******************************************************************************/
+#ifndef UNUSED
+/** Macro to suppress the "unused parameter" warnings. */
+# define UNUSED(x) (void)(x)
+#endif
+/** Macro for printing the configuration bit description with indent. */
+#define PRINT_DESC(fmt, args...) PRINTF(" " fmt "\n", ##args)
+/** Macro for printing the configuration bit result with indent. */
+#define PRINT_RESULT(fmt, args...) PRINTF("  " fmt "\n", ##args)
+
+/*******************************************************************************
+ * Function prototypes
+ ******************************************************************************/
+/**
+ * Read the two Flash Configuration Words, verify and print the read Flash
+ * Configuration Words in a user friendly format.
+ * @note    The print of the read data is done using the @ref PRINTF wrapper.
+ * 
+ * #### Example output (without the asterisks):
+ * @verbatim
+ * Reading Flash Configuration Word 1...
+ * Binary read value:
+ * 111111110011111101111111
+ * Successfully verified the Flash Configuration Word 1 with address 0x00ABFE
+ *  JTAGEN: JTAG Port Enable bit
+ *   [JTAGEN_OFF] JTAG port is disabled
+ *  GCP: General Segment Program Memory Code Protection bit
+ *   [GCP_OFF] Code protection is disabled
+ *  GWRP: General Segment Code Flash Write Protection bit
+ *   [GWRP_OFF] Writes to program memory are allowed
+ *  !DEBUG: Background Debugger Enable bit
+ *   [BKBUG_OFF] Device resets into Operational mode
+ *  ICS<1:0>: Emulator Pin Placement Select bits
+ *   [ICS_PGx1] Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1
+ *  FWDTEN: Watchdog Timer Enable bit
+ *   [FWDTEN_OFF] Watchdog Timer is disabled
+ *  WINDIS: Windowed Watchdog Timer Disable bit
+ *   [WINDIS_ON] Standard Watchdog Timer is enabled
+ *  FWPSA: WDT Prescaler Ratio Select bit
+ *   [FWPSA_PR128] Prescaler ratio of 1:128
+ *  WDTPS<3:0>: Watchdog Timer Postscaler Select bits
+ *   [WDTPS_PS32768] 1:32768
+ * Done Reading Flash Configuration Word 1.
+ * 
+ * Reading Flash Configuration Word 2...
+ * Binary read value:
+ * 111111111111100101101101
+ * Successfully verified the Flash Configuration Word 2 with address 0x00ABFC
+ *  IESO: Internal External Switchover bit
+ *   [IESO_ON] IESO mode (Two-Speed Start-up) is enabled
+ *  WUTSEL<1:0>: Voltage Regulator Standby Mode Wake-up Time Select bits
+ *   [WUTSEL_LEG] Default regulator start-up time is used
+ *  SOSCSEL<1:0>: Secondary Oscillator Power Mode Select bits
+ *   [SOSCSEL_SOSC] Default (High Drive Strength) mode
+ *  FNOSC<2:0>: Initial Oscillator Select bits
+ *   [FNOSC_FRCPLL] Fast RC Oscillator with Postscaler and PLL module (FRCPLL)
+ *  FCKSM<1:0>: Clock Switching and Fail-Safe Clock Monitor Configuration bits
+ *   [FCKSM_CSECMD] Clock switching is enabled, Fail-Safe Clock Monitor is disabled
+ *  OSCIOFCN: OSCO Pin Configuration bit:
+ *   OSCIOFCN has no effect on OSCO/CLKO/RA3
+ *  IOL1WAY: IOLOCK One-Way Set Enable bit
+ *   [IOL1WAY_OFF] The IOLOCK (OSCCON<6>) bit can be set and cleared as needed, provided the unlock sequence has been completed
+ *  I2C1SEL: I2C1 Pin Select bit
+ *   [I2C1SEL_PRI] Use default SCL1/SDA1 pins
+ *  POSCMD<1:0:> Primary Oscillator Configuration bits
+ *   [POSCMOD_XT] XT Oscillator mode is selected
+ * Done Reading Flash Configuration Word 2.
+ * @endverbatim
+ */
+void
+readPICConfigBits( void );
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif /* GETPICCONFIGURATIONBITS_H */
+/* End of file getPICConfigurationBits.h */
---
+++ source/LibraryFiles/src/getPICConfigurationBits.c
@@ -0,0 +1,425 @@
+/*******************************************************************************
+ * Begin of file getPICConfigurationBits.c
+ * Author: jdebruijn
+ * Created on November 16, 2015, 4:11 PM
+ * 
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+ * 
+ * Copyright (c) 2015 Jeroen de Bruijn <vidavidorra@gmail.com>
+ * 
+ * This file is part of LedCube which is released under The MIT License (MIT).
+ * For full license details see file "main.c" or "LICENSE.md" or go to
+ * https://opensource.org/licenses/MIT
+ *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~**/
+/** @file
+ * @brief Gets the configuration bits that are set in the microcontroller.
+ * 
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "getPICConfigurationBits.h"
+
+/*******************************************************************************
+ * Structures
+ ******************************************************************************/
+/** Internal typedef for a 16-bit unsigned word. */
+typedef unsigned short _getPICConfigurationBits_UWord16;
+/** Internal typedef for a 32-bit unsigned word. */
+typedef unsigned long  _getPICConfigurationBits_UWord32;
+/** @cond ignore
+ * Internal typedef union for a 32-bit register. */
+typedef union _tuReg32 {
+  _getPICConfigurationBits_UWord32 Val32;
+
+  struct {
+    _getPICConfigurationBits_UWord16 LW;
+    _getPICConfigurationBits_UWord16 HW;
+  } Word;
+
+  char Val[4];
+} _getPICConfigurationBits_uReg32;
+
+/** Internal structure for the Flash Configuration Word 1. */
+typedef struct tagCW1BITS {
+  union {
+    struct {
+        unsigned WDTPS:4;
+        unsigned FWPSA:1;
+        unsigned :1;
+        unsigned WINDIS:1;
+        unsigned FWDTEN:1;
+        unsigned ICS:2;
+        unsigned :1;
+        unsigned BKBUG:1;
+        unsigned GWRP:1;
+        unsigned GCP:1;
+        unsigned JTAGEN:1;
+    };
+    struct {
+        unsigned WDTPS0:1;
+        unsigned WDTPS1:1;
+        unsigned WDTPS2:1;
+        unsigned :4;
+        unsigned ICS0:1;
+        unsigned ICS1:1;
+    };
+  };
+} CW1BITS;
+
+/** Internal structure for the Flash Configuration Word 2. */
+typedef struct tagCW2BITS {
+  union {
+    struct {
+        unsigned POSCMD:2;
+        unsigned I2C1SEL:1;
+        unsigned :1;
+        unsigned IOL1WAY:1;
+        unsigned OSCIOFCN:1;
+        unsigned FCKSM:2;
+        unsigned FNOSC:3;
+        unsigned SOSCSEL:2;
+        unsigned WUTSEL:2;
+        unsigned IESO:1;
+    };
+    struct {
+        unsigned POSCMD0:1;
+        unsigned POSCMD1:1;
+        unsigned :4;
+        unsigned FCKSM0:1;
+        unsigned FCKSM1:1;
+        unsigned FNOSC0:1;
+        unsigned FNOSC1:1;
+        unsigned FNOSC2:1;
+        unsigned SOSCSEL0:1;
+        unsigned SOSCSEL1:1;
+        unsigned WUTSEL0:1;
+        unsigned WUTSEL1:1;
+    };
+  };
+} CW2BITS;
+
+/** Internal typedef union for the Flash Configuration Word 1. */
+typedef union CWReg1_t {
+  _getPICConfigurationBits_UWord32 Val32;
+
+  struct {
+    _getPICConfigurationBits_UWord16 LW;
+    _getPICConfigurationBits_UWord16 HW;
+  } Word;
+
+  char Val[4];
+  CW1BITS CW1bits;
+} CWReg1_t;
+
+/** Internal typedef union for the Flash Configuration Word 1. */
+typedef union CWReg2_t {
+  _getPICConfigurationBits_UWord32 Val32;
+
+  struct {
+    _getPICConfigurationBits_UWord16 LW;
+    _getPICConfigurationBits_UWord16 HW;
+  } Word;
+
+  char Val[4];
+  CW2BITS CW2bits;
+} CWReg2_t;
+/** @endcond ignore */
+
+/*******************************************************************************
+ * Functions
+ ******************************************************************************/
+/**
+ * Internal function to read a 32-bit value from an address.
+ * 
+ * @note    This gives a compiler warning because a value is not explicitly
+ * returned from the function.
+ * @param   addrhi High word of the address to read from.
+ * @param   addrlo Low word of the address to read from.
+ * @return  UWord32 32-bit value that was at the address of addrhi and addrlo.
+ */
+_getPICConfigurationBits_UWord32
+_getPICConfigurationBits_readLatch( _getPICConfigurationBits_UWord16 addrhi,
+        _getPICConfigurationBits_UWord16 addrlo) {
+    UNUSED(addrhi);
+    UNUSED(addrlo);
+    
+    /*_ReadLatch: ;W0=TBLPAG,W1=Wn - data in W1:W0 */
+    asm("	mov	W0,TBLPAG");
+    asm("	tblrdl [W1],W0");
+    asm("	tblrdh [W1],W1");
+    /* This gives a compiler warning because not explicitly returning a value
+     * from the function, but since this is assembly, do not know how to prevent
+     * warning.
+     * The function is correct, since W1:W0 being used to return the tblrdl,
+     * tblrdh. Could write less efficient code and use a temporary UWord32.
+     */
+}
+
+/**
+ * Internal function to verify that the upper byte of a Flash Configuration
+ * Word is '1111 1111' as it should be according to the PIC24FJ64GA004 Family
+ * Data Sheet.pdf (page 209).
+ * 
+ * @param   _CW 32-bit Configuration Word to verify the upper byte from.
+ * @return  bool True if the upper byte is successfully verified, false else.
+ */
+bool
+_getPICConfigurationBits_verifyConfigUpperByte(
+        _getPICConfigurationBits_UWord32 _CW ) {
+    if((_CW & 0x00FF0000) == 0x00FF0000) {
+        return(true);
+    } else {
+        return(false);
+    }
+}
+
+void
+readPICConfigBits( void ) {
+    /* Base address of CW1: Flash Configuration Word 1 */
+    _getPICConfigurationBits_uReg32 CW1Addr;
+    /* Base address of CW1: Flash Configuration Word 2 */
+    _getPICConfigurationBits_uReg32 CW2Addr;
+    /* Value of CW1: Flash Configuration Word 1 */
+    CWReg1_t CW1;
+    /* Value of CW1: Flash Configuration Word 2 */
+    CWReg2_t CW2;
+    
+    /* For the Configuration Word Addresses see PIC24FJ64GA004 Family Data
+     * Sheet.pdf (page 209, TABLE 24-1).
+     */
+    CW1Addr.Val32 = _CONFIG1_BASE_ADDRESS;
+    CW2Addr.Val32 = _CONFIG2_BASE_ADDRESS;
+        
+    /********** Read and verify Flash Configuration Word 1 ********************/
+    PRINTF("\n\nReading Flash Configuration Word 1...\n");
+    CW1.Val32 = _getPICConfigurationBits_readLatch(CW1Addr.Word.HW,
+            CW1Addr.Word.LW);
+    PRINTF("Binary read value:\n");
+    Uart1_putBits(CW1.Val32, 24);
+    
+    if(_getPICConfigurationBits_verifyConfigUpperByte(CW1.Val32) == true) {
+        PRINTF("\nSuccessfully verified the Flash Configuration Word 1" \
+            " with address 0x%.2X%.4X\n", CW1Addr.Word.HW, CW1Addr.Word.LW);
+        
+        /********** Document Flash Configuration Word 1 results ***************/
+        /* JTAGEN (bit 14) */
+        PRINT_DESC(JTAGEN_DESC);
+        if(CW1.CW1bits.JTAGEN) {
+            PRINT_RESULT(JTAGEN_1);
+        } else {
+            PRINT_RESULT(JTAGEN_0);
+        }
+        /* GCP (bit 13) */
+        PRINT_DESC(GCP_DESC);
+        if(CW1.CW1bits.GCP) {
+            PRINT_RESULT(GCP_1);
+        } else {
+            PRINT_RESULT(GCP_0);
+        }
+        /* GWRP (bit 12) */
+        PRINT_DESC(GWRP_DESC);
+        if(CW1.CW1bits.GWRP) {
+            PRINT_RESULT(GWRP_1);
+        } else {
+            PRINT_RESULT(GWRP_0);
+        }
+        /* !DEBUG (bit 11) */
+        PRINT_DESC(BKBUG_DESC);
+        if(CW1.CW1bits.BKBUG) {
+            PRINT_RESULT(BKBUG_1);
+        } else {
+            PRINT_RESULT(BKBUG_0);
+        }
+        /* ICS (bit 9-8) */
+        PRINT_DESC(ICS_DESC);
+        if(CW1.CW1bits.ICS == 0b11) {
+            PRINT_RESULT(ICS_11);
+        } else if(CW1.CW1bits.ICS == 0b10) {
+            PRINT_RESULT(ICS_10);
+        } else if(CW1.CW1bits.ICS == 0b01) {
+            PRINT_RESULT(ICS_01);
+        } else if(CW1.CW1bits.ICS == 0b00) {
+            PRINT_RESULT(ICS_00);
+        }
+        /* FWDTEN (bit 7) */
+        PRINT_DESC(FWDTEN_DESC);
+        if(CW1.CW1bits.FWDTEN) {
+            PRINT_RESULT(FWDTEN_1);
+        } else {
+            PRINT_RESULT(FWDTEN_0);
+        }
+        /* WINDIS (bit 6) */
+        PRINT_DESC(WINDIS_DESC);
+        if(CW1.CW1bits.WINDIS) {
+            PRINT_RESULT(WINDIS_1);
+        } else {
+            if(CW1.CW1bits.FWDTEN) {
+                PRINT_RESULT(WINDIS_0);
+            } else {
+            PRINT_RESULT(WINDIS_0);
+            PRINTF("  DEBUG: %s:%d:%s(): WARNING: FWDTEN is not '1', this " \
+                    "setting has no effect!", __FILE__, __LINE__, __FUNCTION__);
+            }
+            
+        }
+        /* FWPSA (bit 4) */
+        PRINT_DESC(FWPSA_DESC);
+        if(CW1.CW1bits.FWPSA) {
+            PRINT_RESULT(FWPSA_1);
+        } else {
+            PRINT_RESULT(FWPSA_0);
+        }
+        /* WDTPS (bit 3-0) */
+        PRINT_DESC(WDTPS_DESC);
+        if(CW1.CW1bits.WDTPS == 0b1111) {
+            PRINT_RESULT(WDTPS_1111);
+        } else if(CW1.CW1bits.WDTPS == 0b1110) {
+            PRINT_RESULT(WDTPS_1110);
+        } else if(CW1.CW1bits.WDTPS == 0b1101) {
+            PRINT_RESULT(WDTPS_1101);
+        } else if(CW1.CW1bits.WDTPS == 0b1100) {
+            PRINT_RESULT(WDTPS_1100);
+        } else if(CW1.CW1bits.WDTPS == 0b1011) {
+            PRINT_RESULT(WDTPS_1011);
+        } else if(CW1.CW1bits.WDTPS == 0b1010) {
+            PRINT_RESULT(WDTPS_1010);
+        } else if(CW1.CW1bits.WDTPS == 0b1001) {
+            PRINT_RESULT(WDTPS_1001);
+        } else if(CW1.CW1bits.WDTPS == 0b1000) {
+            PRINT_RESULT(WDTPS_1000);
+        } else if(CW1.CW1bits.WDTPS == 0b0111) {
+            PRINT_RESULT(WDTPS_0111);
+        } else if(CW1.CW1bits.WDTPS == 0b0110) {
+            PRINT_RESULT(WDTPS_0110);
+        } else if(CW1.CW1bits.WDTPS == 0b0101) {
+            PRINT_RESULT(WDTPS_0101);
+        } else if(CW1.CW1bits.WDTPS == 0b0100) {
+            PRINT_RESULT(WDTPS_0100);
+        } else if(CW1.CW1bits.WDTPS == 0b0011) {
+            PRINT_RESULT(WDTPS_0011);
+        } else if(CW1.CW1bits.WDTPS == 0b0010) {
+            PRINT_RESULT(WDTPS_0010);
+        } else if(CW1.CW1bits.WDTPS == 0b0001) {
+            PRINT_RESULT(WDTPS_0001);
+        } else if(CW1.CW1bits.WDTPS == 0b0000) {
+            PRINT_RESULT(WDTPS_0000);
+        }
+    } else {
+        PRINTF("\nVerifying Flash Configuration Word 1 with address" \
+            " 0x%.2X %.4X failed...\n", CW1Addr.Word.HW, CW1Addr.Word.LW);
+    }
+    PRINTF("Done Reading Flash Configuration Word 1.\n");
+    
+    /********** Read and verify Flash Configuration Word 2 ********************/
+    PRINTF("\nReading Flash Configuration Word 2...\n");
+    CW2.Val32 = _getPICConfigurationBits_readLatch(CW2Addr.Word.HW,
+            CW2Addr.Word.LW);
+    PRINTF("Binary read value:\n");
+    Uart1_putBits(CW2.Val32, 24);
+    
+    if(_getPICConfigurationBits_verifyConfigUpperByte(CW2.Val32) == true) {
+        PRINTF("\nSuccessfully verified the Flash Configuration Word 2" \
+            " with address 0x%.2X%.4X\n", CW2Addr.Word.HW, CW2Addr.Word.LW);
+        
+        /********** Document Flash Configuration Word 2 results ***************/
+        /* IESO (bit 15) */
+        PRINT_DESC(IESO_DESC);
+        if(CW2.CW2bits.IESO) {
+            PRINT_RESULT(IESO_1);
+        } else {
+            PRINT_RESULT(IESO_0);
+        }
+        /* WUTSEL (bit 14-13) */
+        PRINT_DESC(WUTSEL_DESC);
+        if(CW2.CW2bits.WUTSEL == 0b11) {
+            PRINT_RESULT(WUTSEL_11);
+        } else if(CW2.CW2bits.WUTSEL == 0b01) {
+            PRINT_RESULT(WUTSEL_01);
+        } else {
+            PRINT_RESULT(WUTSEL_x0);
+        }
+        /* SOSCSEL (bit 12-11) */
+        PRINT_DESC(SOSCSEL_DESC);
+        if(CW2.CW2bits.SOSCSEL == 0b11) {
+            PRINT_RESULT(SOSCSEL_11);
+        } else if(CW2.CW2bits.SOSCSEL == 0b01) {
+            PRINT_RESULT(SOSCSEL_01);
+        } else {
+            PRINT_RESULT(SOSCSEL_x0);
+        }
+        /* FNOSC (bit 10-8) */
+        PRINT_DESC(FNOSC_DESC);
+        if(CW2.CW2bits.FNOSC == 0b111) {
+            PRINT_RESULT(FNOSC_111);
+        } else if(CW2.CW2bits.FNOSC == 0b110) {
+            PRINT_RESULT(FNOSC_110);
+        } else if(CW2.CW2bits.FNOSC == 0b101) {
+            PRINT_RESULT(FNOSC_101);
+        } else if(CW2.CW2bits.FNOSC == 0b100) {
+            PRINT_RESULT(FNOSC_100);
+        } else if(CW2.CW2bits.FNOSC == 0b011) {
+            PRINT_RESULT(FNOSC_011);
+        } else if(CW2.CW2bits.FNOSC == 0b010) {
+            PRINT_RESULT(FNOSC_010);
+        } else if(CW2.CW2bits.FNOSC == 0b001) {
+            PRINT_RESULT(FNOSC_001);
+        } else if(CW2.CW2bits.FNOSC == 0b000) {
+            PRINT_RESULT(FNOSC_000);
+        }
+        /* FCKSM (bit 7-6) */
+        PRINT_DESC(FCKSM_DESC);
+        if(CW2.CW2bits.FCKSM == 0b01) {
+            PRINT_RESULT(FCKSM_01);
+        } else if(CW2.CW2bits.FCKSM == 0b00) {
+            PRINT_RESULT(FCKSM_00);
+        } else {
+            PRINT_RESULT(FCKSM_1x);
+        }
+        /* OSCIOFCN (bit 5) */
+        PRINT_DESC(OSCIOFCN_DESC);
+        if(CW2.CW2bits.POSCMD == 0b11 || CW2.CW2bits.POSCMD == 00) {
+            if(CW2.CW2bits.OSCIOFCN) {
+                PRINT_RESULT(OSCIOFCN_1);
+            } else {
+                PRINT_RESULT(OSCIOFCN_0);
+            }
+        } else if(CW2.CW2bits.POSCMD == 0b10 || CW2.CW2bits.POSCMD == 01) {
+            PRINT_RESULT(OSCIOFCN_x);
+        }
+        /* IOL1WAY (bit 4) */
+        PRINT_DESC(IOL1WAY_DESC);
+        if(CW2.CW2bits.IOL1WAY) {
+            PRINT_RESULT(IOL1WAY_1);
+        } else {
+            PRINT_RESULT(IOL1WAY_0);
+        }
+        /* I2C1SEL (bit 2) */
+        PRINT_DESC(I2C1SEL_DESC);
+        if(CW2.CW2bits.I2C1SEL) {
+            PRINT_RESULT(I2C1SEL_1);
+        } else {
+            PRINT_RESULT(I2C1SEL_0);
+        }
+        /* POSCMD (bit 1-0) */
+        PRINT_DESC(POSCMD_DESC);
+        if(CW2.CW2bits.POSCMD == 0b11) {
+            PRINT_RESULT(POSCMD_11);
+        } else if(CW2.CW2bits.POSCMD == 0b10) {
+            PRINT_RESULT(POSCMD_10);
+        } else if(CW2.CW2bits.POSCMD == 0b01) {
+            PRINT_RESULT(POSCMD_01);
+        }else if(CW2.CW2bits.POSCMD == 0b00) {
+            PRINT_RESULT(POSCMD_00);
+        }
+    } else {
+        PRINTF("\nVerifying Flash Configuration Word 2 with address" \
+            " 0x%.2X %.4X failed...\n", CW2Addr.Word.HW, CW2Addr.Word.LW);
+    }
+    PRINTF("Done Reading Flash Configuration Word 2.\n\n\n");
+    
+    return;
+}
+
+/* End of file getPICConfigurationBits.c */
