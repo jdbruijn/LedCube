ca668f440334865b2d27b8715fa16ff53254c548
Jeroen de Bruijn <vidavidorra@gmail.com>
2015-11-29 22:39:16 +0100

    Removed dummy function and used cast to void 0 to don't do anything when DEBUG is not defined.

--- source/LibraryFiles/Debug.h
+++ source/LibraryFiles/Debug.h
@@ -55,8 +55,6 @@ extern "C" {
 #     define _DEBUG_FUNC_INIT_FLAG 0
 #   endif
 #else
-/* Wrapper for PRINTF to a dummy printf function which doesn't do anything */
-#   define PRINTF _dummy_printf
 #   define _DEBUG_FLAG 0
 #   define _DEBUG_FUNC_INIT_FLAG 0
 #endif
@@ -68,15 +66,10 @@ extern "C" {
 /**
  * Print formatted debugging data with prefix filename, line and function.
  * 
- * @See     Uartx_printf() documentation for more information about the
+ * @See     Uartx_printf() documentation for more information about the function
  * parameters.
- * @param   fmt, format C string that contains the text to be written.
- * @param   args..., additional arguments.
- * @Example <code>// If the following line was placed in a file called test.c,
- * on line 51 and in a function called debugTest().\n
- * DEBUG_PRINTF("Testing %s printing %d...", "debug", 123);\n
- * // Prints (without the quotemarks): "&#32;DEBUG: src/test.c:51:debugTest():
- * Testing debug printing 123..."\n</code>
+ * @param   fmt Format C string that contains the text to be written.
+ * @param   args... Additional arguments.
  */
 #   define DEBUG_PRINTF(fmt, args...) do{ if(_DEBUG_FLAG) { \
                 PRINTF(" DEBUG: %s:%d:%s(): " fmt "\n", __FILE__, __LINE__, \
@@ -84,31 +77,38 @@ extern "C" {
                 } }while(0)
 
 /**
- * Print formatted debugging data for a function call.
+ * Print formatted debugging data from a function.
  * 
- * @See     Uartx_printf() documentation for more information about the
+ * @See     Uartx_printf() documentation for more information about the function
  * parameters.
- * @param   fmt, format C string that contains the text to be written.
- * @param   args..., additional arguments.
- * @Example <code>// function declaration: \n
- * void test( uint8_t *_ptr, uint8_t _v ) {\n
- * &#32 DEBUG_PRINTF_FUNCTION_CALL("%p, %u", _ptr, _v);\n
- * &#32 // user code\n
- * &#32 return;\n
- * }\n\n
- * uint8_t value = 44;\n
- * test( &value, value ); // function call\n
- * // Prints (without the quotemarks): " DEBUG: test(b06, 44)"
- * </code>
+ * @param   fmt Format C string that contains the text to be written.
+ * @param   args... Additional arguments.
+ */
+#   define DEBUG_PRINTF_FUNCTION(fmt, args...) do{ if(_DEBUG_FLAG) { \
+                PRINTF(" DEBUG: %s(): " fmt "\n", __FUNCTION__ , ##args); \
+                } }while(0)
+
+/**
+ * Print formatted debugging data for a function call, which can include the
+ * function parameters.
+ * 
+ * @See     Uartx_printf() documentation for more information about the function
+ * parameters.
+ * @param   fmt Format C string that contains the text to be written.
+ * @param   args... Additional arguments.
  */
 #   define DEBUG_PRINTF_FUNCTION_CALL(fmt, args...) do{ if(_DEBUG_FLAG) { \
                 PRINTF(" DEBUG: %s(" fmt ")\n", __FUNCTION__ , ##args); \
                 } }while(0)
 
-#   define DEBUG_PRINTF_FUNCTION(fmt, args...) do{ if(_DEBUG_FLAG) { \
-                PRINTF(" DEBUG: %s(): " fmt "\n", __FUNCTION__ , ##args); \
-                } }while(0)
-
+/**
+ * Print a function initialize complete message.
+ * 
+ * @See     Uartx_printf() documentation for more information about the function
+ * parameters.
+ * @param   fmt Format C string that contains the text to be written.
+ * @param   args... Additional arguments.
+ */
 #   define DEBUG_PRINTF_FUNCTION_INITIALIZE_COMPLETE() if(_DEBUG_FUNC_INIT_FLAG) \
                 DEBUG_PRINTF_FUNCTION("Initialize complete...");
 
@@ -117,21 +117,12 @@ extern "C" {
                 __FILE__, __LINE__, __FUNCTION__, ##args); \
                 } }while(0)
 #else /* DEBUG */
+#   define PRINTF(fmt, args...) ((void)0)
 #   define DEBUG_PRINTF(fmt, args...) ((void)0)
+#   define DEBUG_PRINTF_FUNCTION(fmt, args...)  ((void)0)
 #   define DEBUG_PRINTF_FUNCTION_CALL(fmt, args...)  ((void)0)
 #   define DEBUG_PRINTF_FUNCTION_INITIALIZE_COMPLETE() ((void)0)
 #endif /* DEBUG */
-
-/*******************************************************************************
- * Function prototypes
- ******************************************************************************/
-/**
- * Dummy printf function that is used if debugging is disabled.
- * 
- * @See     Uartx_printf() documentation for information about the parameters.
- */
-void
-_dummy_printf( const char *format, ... );
 
 #ifdef	__cplusplus
 }
