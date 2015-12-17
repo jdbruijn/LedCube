b8d4b607ad30c5dfa8a5df6fc1b7b36cef351ed9
Jeroen de Bruijn <vidavidorra@gmail.com>
2015-12-17 21:38:57 +0100

    Closes #11 by changing the license in all source files.
    See comment in #11 for the log.

--- source/LibraryFiles/Delay.h
+++ source/LibraryFiles/Delay.h
@@ -5,12 +5,23 @@
  * 
  *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
  * 
- * Copyright (c) 2015 Jeroen de Bruijn <vidavidorra@gmail.com>
+ * This file is part of LedCube.
  * 
- * This file is part of LedCube which is released under The MIT License (MIT).
- * For full license details see file "main.c" or "LICENSE" or go to
- * https://opensource.org/licenses/MIT
+ * Copyright (c) 2015  Jeroen de Bruijn  <vidavidorra@gmail.com>
  * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~**/
 /** @file
  * @brief Delay macros and wrappers to the delay macros in libpic32.h.
