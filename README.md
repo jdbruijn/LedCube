a6e3a698f8a5870e44b61d66637b66212c605c71
Jeroen de Bruijn <vidavidorra@gmail.com>
2015-12-18 11:01:24 +0100

    Forgot to change license file and readme. (#11)
    So this closes #11 and not the previous commit (b8d4b60)

--- README.md
+++ README.md
@@ -1,29 +1,27 @@
-LedCube
-=======
-
-[![License MIT][badge-license]][license]
+# LedCube
+[![License GPLv3][badge-license]][license]
 [![Travis build status][badge-travis]][travis]
 
 LedCube is a project of making an 8x8x8 RGB LED cube. This repository contains the drawings, design, overview of the hardware and last but not least the code to control this 8x8x8 RGB LED cube.
 
 LedCube is written in C and uses various peripherals like SPI, UART.
 
-![Alt text](/Docs/3D Model/LedCube 3D Model.bmp?raw=true "LedCube 3D Model")
+![LedCube 3D Model][ledcube-3d-model]
 
-LedCube Visualisation
-=====================
+## LedCube Visualisation
 [LedCube Visualisation](https://github.com/vidavidorra/LedCube-Visualisation/) is a Windows program that visualizes a 8x8x8 RGB LED cube and can be used to visualize a LED cube without risking of damaging the actual LED cube. In the future this program can possibly also be used to program the LedCube by visually programming sequences.<br />
 
-- Documentation can be found in the [wiki](https://github.com/vidavidorra/LedCube/wiki) and in the [docs](Docs) directory.
+- Documentation can be found in the [wiki](https://github.com/vidavidorra/LedCube/wiki), in the [LedCube-Documentation project](https://github.com/vidavidorra/LedCube-Documentation/tree/master/LedCube_docs) and on the [LedCube Documentation site](http://vidavidorra.github.io/LedCube-Documentation).
 - [Pull requests](https://github.com/vidavidorra/LedCube/pulls) and [bug reports](https://github.com/vidavidorra/LedCube/issues) are welcome!
 
-Licensing
-=========
-LedCube is free software, released under the [MIT license](http://opensource.org/licenses/MIT), as published by the Open Source Initiative.
-Modification and redistribution are permitted according to the terms of the MIT license.
+# Licensing
+GNU General Public License version 3 or later, as published by the Free Software Foundation.
+Modification and redistribution are permitted according to the terms of the GPL.
 The license can be found in the `LICENSE` file.
 
-[badge-license]: https://img.shields.io/badge/license-MIT-blue.svg
-[license]: https://github.com/vidavidorra/LedCube/blob/master/LICENSE
-[badge-travis]: https://travis-ci.org/vidavidorra/LedCube.svg
-[travis]: https://travis-ci.org/vidavidorra/LedCube
+
+[badge-license]:                        https://img.shields.io/badge/license-GPLv3-blue.svg
+[license]:                              https://github.com/vidavidorra/LedCube/blob/master/LICENSE
+[badge-travis]:                         https://travis-ci.org/vidavidorra/LedCube.svg
+[travis]:                               https://travis-ci.org/vidavidorra/LedCube
+[ledcube-3d-model]:                     https://github.com/vidavidorra/LedCube-Documentation/blob/master/LedCube_docs/3D%20Model/LedCube%203D%20Model.bmp?raw=true
--- travis/travis-install-dependencies.sh
+++ travis/travis-install-dependencies.sh
@@ -2,6 +2,25 @@
 #
 # Install the software needed for a build.
 #
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+# 
+# Copyright (c) 2015  Jeroen de Bruijn  <vidavidorra@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+# 
 # Downloads the software from Microchip and saves them in a folder outside
 # of the project directory. Than extracs and installs them.
 # 
@@ -10,7 +29,7 @@
 #  MPLAB® XC16 Compiler: v1.25
 #
 
-# Save the current working directory
+# Save the current working directory.
 pushd .
 
 # Create a folder for the installation files.
@@ -18,48 +37,49 @@ mkdir ../install-dependencies
 cd ../install-dependencies
 
 ### IDE
-# Install MPLAB® X IDE v3.15
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-echo - Downloading MPLAB® X IDE v3.15... -
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+# Install MPLAB X IDE v3.15
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "- Downloading MPLAB X IDE v3.15... +"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
 wget http://ww1.microchip.com/downloads/en/DeviceDoc/MPLABX-v3.15-linux-installer.tar
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-echo - Download finished...                          -
-echo - Unpacking MPLABX-v3.15-linux-installer.tar... -
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "- Download finished...                          -"
+echo "- Unpacking MPLABX-v3.15-linux-installer.tar... -"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 tar -xvf MPLABX-v3.15-linux-installer.tar
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-echo - Unpack finished...                     +
-echo - Installing MPLAB® X IDE v3.15...       +
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "- Unpack finished...              -"
+echo "- Installing MPLAB X IDE v3.15... -"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 sudo ./MPLABX-v3.15-linux-installer.sh -- --mode unattended
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-echo - Installation of MPLAB® X IDE v3.15 finished... +
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "- Installation of MPLAB X IDE v3.15 finished... -"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 echo 
 
 ### Compiler
-# Install MPLAB® XC16 Compiler v1.25
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-echo - Downloading MPLAB® XC16 Compiler v1.25 +
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# Install MPLAB XC16 Compiler v1.25
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "- Downloading MPLAB XC16 Compiler v1.25 -"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 wget http://ww1.microchip.com/downloads/en/DeviceDoc/xc16-v1.25-full-install-linux-installer.run
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-echo - Download finished...        -
-echo - Adding excecution rights... -
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "- Download finished...        -"
+echo "- Adding excecution rights... -"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 chmod +x xc16-v1.25-full-install-linux-installer.run
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-echo - Installing MPLAB® XC16 Compiler v1.25 -
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "- Installing MPLAB XC16 Compiler v1.25 +"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
 sudo ./xc16-v1.25-full-install-linux-installer.run --mode unattended --netservername dontknow
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-echo - Installation of MPLAB® XC16 Compiler v1.25 finished... +
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "- Installation of MPLAB XC16 Compiler v1.25 finished... -"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo 
 
 # Return to the saved working directory
 popd
 
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-echo - All installations finished +
-echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "- All installations finished +"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
